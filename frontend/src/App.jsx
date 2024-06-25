136ee2640c3a27024e889b46dd2269ee9f67bbdf
Rumanda Senanayake <rumandasenanayake@gmail.com>
2024-06-25 22:25:17 +0530

    Food Display & Food Item

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Home from './Pages/Home/Home';
 import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
     <div className="app">
       <NavBar />
+      <Routes>
+      <Route path='/' element={<Home />}/>
+      </Routes>
     </div>
   );
 };
